b8c51a3b85e563caa90a2f710299f3add5b38f07
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-04-25 14:33:36 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
-
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import React, {useState} from 'react';
+import './App.js';
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
 
 function App() {
+const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
+  const appClass = isDarkMode ? "App dark" : "App light";
+
+  function handleDarkModeToggle() {
+    setIsDarkMode(!isDarkMode);
+  }
+
   return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+    <div className={appClass}>
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={handleDarkModeToggle}>Toggle Dark Mode</button>
+      </header>
+      <ShoppingList items={itemData} />
     </div>
   );
 }
